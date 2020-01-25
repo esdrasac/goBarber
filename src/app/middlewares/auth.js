83d8dd79f12478426e156b14e01fcdd8ce7f4f74
Esdras Aguilar <esdras_ac2010@hotmail.com>
2020-01-25 11:32:42 -0300

    add middlewares

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,7 +1,24 @@
-module.exports = (req, res, next) => {
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+
+const authConfig = require('../../config/auth');
+
+module.exports = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  console.log(authHeader);
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
 
-  return next();
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
 };
--- src/routes.js
+++ src/routes.js
@@ -6,8 +6,11 @@ const authMiddleware = require('./app/middlewares/auth');
 
 const routes = new Router();
 
-routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
-routes.put('/users', authMiddleware, UserController.update);
+routes.post('/users', UserController.store);// Create user
+routes.post('/sessions', SessionController.store); // Gerenate token
+
+
+routes.use(authMiddleware); // Global route
+routes.put('/users', UserController.update);
 
 module.exports = routes;
