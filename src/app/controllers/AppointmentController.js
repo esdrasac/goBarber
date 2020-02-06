73e87154c72157494824078c58da79aca487cb3f
Esdras Aguilar <esdras_ac2010@hotmail.com>
2020-02-06 19:14:17 -0300

    Validation appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,35 @@
 const Yup = require('yup');
+const { startOfHour, parseISO, isBefore } = require('date-fns');
 
 const User = require('../models/User');
+const File = require('../models/File');
 const Appointment = require('../models/Appointment');
 
 class AppointmentController {
+  async index(req, res) {
+    const appointment = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json(appointment);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -23,6 +49,24 @@ class AppointmentController {
     if (!checkProvider) {
       return res.status(400)
         .json({ error: 'You can only create appointments with providers' });
+    }
+
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past date are not permited' });
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res.status(400).json({ error: 'Appointment date is not available' });
     }
 
     const appointment = await Appointment.create({
